9d8b035322ff5f2f544d20400594c360462699c1
SKYE <1446207485@qq.com>
2024-03-04 11:02:45 +0800

    feat : user.service.js update statement

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,9 +1,19 @@
+/*
+ * @FileDescription: 用户服务
+ * @Author: zyj
+ * @Date: 2024-03-04 10:30:37
+ * @LastEditors: zyj
+ * @LastEditTime: 2024-03-04 11:00:35
+ * @FilePath: \koa-app\src\service\user.service.js
+ * @Description:
+ */
+
 const connections = require("../app/database");
 /** 向数据库中添加数据 */
 class userService {
   async create({ name, password }) {
     /** 将用户数据保存至数据库中 */
-    const statement = `INSERT INTO users (name,pwd) VALUES (?,?); `;
+    const statement = `INSERT INTO users (name,password) VALUES (?,?); `;
     const result = await connections.execute(statement, [name, password]);
     /** 返回数据 */
     return result;
