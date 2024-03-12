bf5ecf02b12c7b6477617258443dc8663a652c35
zhangyj <zhangyj@nengfeng.com.cn>
2024-03-12 10:22:34 +0800

    feat : Verify if the user exists

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,12 +5,13 @@ const userService = require("../service/user.service");
 const verifyLogin = async (ctx, next) => {
   const { name } = ctx.request.body;
   /** 校验登录账户存在数据库中 */
-  const userInfo = await userService.getUserByName(name);
-  if (!userInfo[0]) {
+  const result = await userService.getUserByName(name);
+  const userInfo = result[0];
+  if (!userInfo) {
     const error = new Error(USER_DOES_NOT_EXISTS);
     return ctx.app.emit("error", error, ctx);
   }
-
+  console.log(userInfo, "userinfo");
   /** 校验登录账户与密码一致 */
   await next();
 };
