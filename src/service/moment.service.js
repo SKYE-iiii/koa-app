b598a0fa37baf134ce7ae34ac46ba67cc9cefc5c
zhangyj <zhangyj@nengfeng.com.cn>
2024-03-22 16:02:49 +0800

    fix : sql statement  terminator

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -3,7 +3,7 @@ const connections = require("../app/database");
 class MomentService {
   /** 添加动态语句 */
   async create({ content, userId }) {
-    const statement = `INSERT INTO moments (content,userId) VALUES (?,?)`;
+    const statement = `INSERT INTO moments (content,userId) VALUES (?,?);`;
     const result = await connections.execute(statement, [content, userId]);
     return result;
   }
