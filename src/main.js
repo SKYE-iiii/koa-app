02b0cfce368a65c0b312acfaea56d4dda2eb0b8a
zhangyj <zhangyj@nengfeng.com.cn>
2024-04-16 11:33:25 +0800

    feat : modify configuration

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Description:
+ * @Author: zyj
+ * @Date: 2024-03-04 10:30:37
+ * @LastEditors: zyj
+ * @LastEditTime: 2024-04-16 11:08:12
+ * @FilePath: \koa-app\src\main.js
+ */
 const app = require("./app/index.js");
 const config = require("./app/config.js");
 /** 注册database */
